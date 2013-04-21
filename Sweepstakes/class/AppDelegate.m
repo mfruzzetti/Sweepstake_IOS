8d7bfdc9b975674ce31aa26f408907b36b659af1
Ashish Agrawal <ashish.malviya@canopusinfosystems.com>
2013-04-22 00:57:46 +0530

    Implement Parse API app and client key

--- Sweepstakes/class/AppDelegate.m
+++ Sweepstakes/class/AppDelegate.m
@@ -11,6 +11,8 @@
 #import "ViewController.h"
 #import "TapjoyConnect.h"
 #import "vunglepub.h"
+#import "Parse/Parse.h"
+
 @implementation AppDelegate
 @synthesize window = _window;
 @synthesize viewController = _viewController;
@@ -40,7 +42,7 @@
     //data.age             = 36;
     //data.gender          = VGGenderFemale;
     data.adOrientation   = VGAdOrientationPortrait;
-  //  data.locationEnabled = TRUE;
+    //  data.locationEnabled = TRUE;
     
     // start vungle publisher library
     [VGVunglePub startWithPubAppID:appID userData:data];
@@ -48,11 +50,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
- 
-
+    
+    
     [self vungleStart];
     
-    // Tapjoy 
+    // Tapjoy
     [TapjoyConnect requestTapjoyConnect:@"89f9325f-f56d-44d4-954a-09fcd4cf5e16" secretKey:@"7wOYg93VuqQjg3OeFCWt"];
     
     
@@ -73,9 +75,9 @@
 		NSLog(@"Open DB");
     }
     
-   // NSLog(@"Fluryy->%@",[Flurry getFlurryAgentVersion]);
+    // NSLog(@"Fluryy->%@",[Flurry getFlurryAgentVersion]);
     //[FlurryAds initialize:self.window.rootViewController];
-   // [Flurry startSession:@"NYXBTMDNMJNM8YQRXDY4"];
+    // [Flurry startSession:@"NYXBTMDNMJNM8YQRXDY4"];
     [Flurry startSession:@"QMMQM6TN6S9DJP8G9963"];
     
     // Optional: automatically send uncaught exceptions to Google Analytics.
@@ -91,22 +93,20 @@
     [tracker setCampaignUrl:@"utm_campaign=my_campaign&utm_source=google&utm_medium=cpc&utm_term=my_keyword&utm_content=ad_variation1"];
     
     
-    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
-    
-   // NSLog(@"--->%@",[[[launchOptions objectForKey:@"UIApplicationLaunchOptionsRemoteNotificationKey"] objectForKey:@"aps"] objectForKey:@"alert"]);
+    // NSLog(@"--->%@",[[[launchOptions objectForKey:@"UIApplicationLaunchOptionsRemoteNotificationKey"] objectForKey:@"aps"] objectForKey:@"alert"]);
     [[UIApplication sharedApplication]cancelAllLocalNotifications];
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
-   
     
-//    NSString* versionNum = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
-//    NSLog(@"--->%@",versionNum);
-//    if (![versionNum isEqualToString:@"1.1"]) {
-//       NSLog(@"--->%d", [[[NSUserDefaults standardUserDefaults] stringForKey:@"count"]intValue]) ;
-//        int count = [[[NSUserDefaults standardUserDefaults] stringForKey:@"count"]intValue]+1;
-//        
-//        [[NSUserDefaults standardUserDefaults] setObject:[NSString stringWithFormat:@"%d",count] forKey:@"count"];
-//        [[NSUserDefaults standardUserDefaults] synchronize];
-//    }
+    
+    //    NSString* versionNum = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    //    NSLog(@"--->%@",versionNum);
+    //    if (![versionNum isEqualToString:@"1.1"]) {
+    //       NSLog(@"--->%d", [[[NSUserDefaults standardUserDefaults] stringForKey:@"count"]intValue]) ;
+    //        int count = [[[NSUserDefaults standardUserDefaults] stringForKey:@"count"]intValue]+1;
+    //
+    //        [[NSUserDefaults standardUserDefaults] setObject:[NSString stringWithFormat:@"%d",count] forKey:@"count"];
+    //        [[NSUserDefaults standardUserDefaults] synchronize];
+    //    }
     
     if (launchOptions != nil)
 	{
@@ -114,16 +114,16 @@
 		if (userInfo != nil)
 		{
             /*
-            FMDatabase *db=[FMDatabase databaseWithPath:[Utility getDatabasePath]];
-            [db open];
-            FMResultSet *results=[db executeQuery:@"SELECT * FROM login_master where rowid=1"];
-            while ([results next]) 
-            {
-                pushid = [[NSString alloc]initWithFormat:@"%@",[results stringForColumn:@"userid"]] ;
-            }
-            [results close];
-            [db close];
-               */    
+             FMDatabase *db=[FMDatabase databaseWithPath:[Utility getDatabasePath]];
+             [db open];
+             FMResultSet *results=[db executeQuery:@"SELECT * FROM login_master where rowid=1"];
+             while ([results next])
+             {
+             pushid = [[NSString alloc]initWithFormat:@"%@",[results stringForColumn:@"userid"]] ;
+             }
+             [results close];
+             [db close];
+             */
             self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
             // Override point for customization after application launch.
             self.viewController = [[[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil] autorelease];
@@ -141,94 +141,109 @@
             
             if ([dc getData])
             {
-            WinnerlistViewController *gotowinner = [[WinnerlistViewController alloc]initWithNibName:@"WinnerlistViewController" bundle:nil];
-            gotowinner.userid = [[dc getData]objectAtIndex:0];
-            [navigation pushViewController:gotowinner animated:YES];
+                WinnerlistViewController *gotowinner = [[WinnerlistViewController alloc]initWithNibName:@"WinnerlistViewController" bundle:nil];
+                gotowinner.userid = [[dc getData]objectAtIndex:0];
+                [navigation pushViewController:gotowinner animated:YES];
             }
             
 			//NSLog(@"Launched from push notification: %@", userInfo);
             
-          //  UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"launchoption" message:[NSString stringWithFormat:@"%@",[[userInfo objectForKey:@"aps"]objectForKey:@"sub"]] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-         //   [alert show];
-         //   [alert release];
-           
+            //  UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"launchoption" message:[NSString stringWithFormat:@"%@",[[userInfo objectForKey:@"aps"]objectForKey:@"sub"]] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+            //   [alert show];
+            //   [alert release];
+            
         }
 	}
     else {
         
-    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
-    self.splashview = [[[SplashViewController alloc] initWithNibName:@"SplashViewController" bundle:nil] autorelease];
-    UINavigationController *navigation =[[UINavigationController alloc]initWithRootViewController:self.splashview];
+        //Set parse application id and client key
+        [Parse setApplicationId:@"PBfg87M0UsWl3fCsd7dCz9iHQ7aoYGiJoIDfKFWo"
+                      clientKey:@"kiGqEPQuq2GPn2GSM1uz2SS1jwARAsd4gvlgaFKJ"];
+        //To track statistics around application opens
+        [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+        
+        
+        self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+        // Override point for customization after application launch.
+        self.splashview = [[[SplashViewController alloc] initWithNibName:@"SplashViewController" bundle:nil] autorelease];
+        UINavigationController *navigation =[[UINavigationController alloc]initWithRootViewController:self.splashview];
         DDMenuController *rootController = [[DDMenuController alloc] initWithRootViewController:navigation];
         _menuController = rootController;
         MenuViewController *leftController = [[MenuViewController alloc] initWithNibName:@"MenuViewController" bundle:nil];
         _menuController.leftViewController = leftController;
-
-     navigation.navigationBar.hidden=YES;
-    self.window.rootViewController = rootController;
         
-    self.window.backgroundColor = [UIColor clearColor];
-    [self.window makeKeyAndVisible];
-   
+        navigation.navigationBar.hidden=YES;
+        self.window.rootViewController = rootController;
+        
+        self.window.backgroundColor = [UIColor clearColor];
+        [self.window makeKeyAndVisible];
+        
+        //Register app for remote notifications
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
+         UIRemoteNotificationTypeAlert|
+         UIRemoteNotificationTypeSound];
+        
     }
-   
-   
-   
+    
+    
+    
     return YES;
 }
 
 
 
-- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken { 
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     
-  
+    
     tokenstring =[NSString stringWithFormat:@"%@",deviceToken];
-
+    
     tokenstring = [tokenstring stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
 	tokenstring = [[NSString alloc]initWithFormat:@"%@",[tokenstring stringByReplacingOccurrencesOfString:@" " withString:@""]];
-  
-
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
+    
 }
 
 
 
-- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err { 
+- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
     
     NSString *str = [NSString stringWithFormat: @"Error: %@", err];
-    NSLog(@"%@",str);    
+    NSLog(@"%@",str);
     
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-     [dc =[LLDataControll alloc]init];
+    [dc =[LLDataControll alloc]init];
     for (id key in userInfo) {
-      //  NSLog(@"key: %@, value: %@", key, [userInfo objectForKey:key]);
-       
+        //  NSLog(@"key: %@, value: %@", key, [userInfo objectForKey:key]);
+        
         // UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"remotenotification" message:[NSString stringWithFormat:@"%@",[[userInfo objectForKey:@"aps"]objectForKey:@"sub"]] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         /*
-        FMDatabase *db=[FMDatabase databaseWithPath:[Utility getDatabasePath]];
-        [db open];
-        FMResultSet *results=[db executeQuery:@"SELECT * FROM login_master where rowid=1"];
-        while ([results next]) 
-        {
-            pushid = [[NSString alloc]initWithFormat:@"%@",[results stringForColumn:@"userid"]] ;
-           // UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"remotenotification" message:[NSString stringWithFormat:@"idddd->%@",[results stringForColumn:@"userid"]] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-          //  [alert show];
-          //  [alert release];
-        }
-        [results close];
-        [db close];
-        */
+         FMDatabase *db=[FMDatabase databaseWithPath:[Utility getDatabasePath]];
+         [db open];
+         FMResultSet *results=[db executeQuery:@"SELECT * FROM login_master where rowid=1"];
+         while ([results next])
+         {
+         pushid = [[NSString alloc]initWithFormat:@"%@",[results stringForColumn:@"userid"]] ;
+         // UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"remotenotification" message:[NSString stringWithFormat:@"idddd->%@",[results stringForColumn:@"userid"]] delegate:Nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+         //  [alert show];
+         //  [alert release];
+         }
+         [results close];
+         [db close];
+         */
         UIApplicationState state = [application applicationState];
         if (state == UIApplicationStateActive)
         {
-             UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Push Notification" message:[NSString stringWithFormat:@"%@",[[userInfo objectForKey:@"aps"]objectForKey:@"alert"]] delegate:self cancelButtonTitle:@"VIEW" otherButtonTitles:@"CANCEL", nil];
+            UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Push Notification" message:[NSString stringWithFormat:@"%@",[[userInfo objectForKey:@"aps"]objectForKey:@"alert"]] delegate:self cancelButtonTitle:@"VIEW" otherButtonTitles:@"CANCEL", nil];
             [alert show];
             [alert release];
-           
+            
         }else if (state == UIApplicationStateInactive) {
-           
+            
             self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
             // Override point for customization after application launch.
             self.viewController = [[[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil] autorelease];
@@ -243,7 +258,7 @@
             
             self.window.backgroundColor = [UIColor clearColor];
             [self.window makeKeyAndVisible];
-
+            
             if ([dc getData])
             {
                 WinnerlistViewController *gotowinner = [[WinnerlistViewController alloc]initWithNibName:@"WinnerlistViewController" bundle:nil];
@@ -252,13 +267,13 @@
             }
             
         }
-
+        
         
     }
     [[UIApplication sharedApplication]cancelAllLocalNotifications];
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
-  //  NSLog(@"Alert message: %@",[[userInfo valueForKey:@"aps"] valueForKey:@"alert"]);
-
+    //  NSLog(@"Alert message: %@",[[userInfo valueForKey:@"aps"] valueForKey:@"alert"]);
+    
 }
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     if (buttonIndex == 0)
@@ -276,7 +291,7 @@
             gotowinner.userid = [[dc getData]objectAtIndex:0];
             [navigation pushViewController:gotowinner animated:YES];
         }
-
+        
         
 	}
     
@@ -299,12 +314,12 @@
 
 -(void) createAndCheckDatabase
 {
-    BOOL success; 
+    BOOL success;
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     success = [fileManager fileExistsAtPath:databasePath];
     
-    if(success) return; 
+    if(success) return;
     
     NSString *databasePathFromApp = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:self.databaseName];
     [fileManager copyItemAtPath:databasePathFromApp toPath:databasePath error:nil];
@@ -320,10 +335,10 @@
                annotation:annotation]) {
         return YES;
     }
-   //Apsalar Analytics
+    //Apsalar Analytics
     [Apsalar startSession:@"bventures" withKey:@"YOynplNg" andURL:url];
     return YES;
-
+    
     // Handle Google+ sign-in button URL.
     return NO;
 }
@@ -397,12 +412,12 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-   
+    
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
 }
@@ -410,7 +425,7 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-   
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -418,9 +433,9 @@
     NSLog(@"applicationDidBecomeActive");
     //Apsalar Analytics
     [Apsalar startSession:@"bventures" withKey:@"YOynplNg"];
-//    [SponsorPaySDK startForAppId:@"10780"
-//                          userId:nil
-//                   securityToken:@"c2732013599a043f3c20f91aaf6cc614"];
+    //    [SponsorPaySDK startForAppId:@"10780"
+    //                          userId:nil
+    //                   securityToken:@"c2732013599a043f3c20f91aaf6cc614"];
     
     NSString *credentialsToken =
     [SponsorPaySDK startForAppId:@"10780"
@@ -433,9 +448,9 @@
         
         [SponsorPaySDK reportActionCompleted:@"A88497"
                               forCredentials:credentialsToken];
-      
+        
     } @catch (NSException *exception) {
-       
+        
     }
 }
 
